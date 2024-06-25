956bdfb7e8f43601d033d2bb0d195a1ec8702d5b
Nolan <bourrel.nolan@gmail.com>
2024-06-25 12:03:22 +0200

    push apres merge

--- modele.py
+++ modele.py
@@ -1,20 +1,20 @@
-import mysql.connector
+# import mysql.connector
 
 
 
 
-mydb = mysql.connector.connect(
-    port = 8889,
-    host = "localhost",
-    user="root",
-    password="root"
-    database = "",
-    )
+# mydb = mysql.connector.connect(
+#     port = 8889,
+#     host = "localhost",
+#     user="root",
+#     password="root"
+#     database = "",
+#     )
 
-mycursor = mydb.cursor()
+# mycursor = mydb.cursor()
 
 
 
-def get_users():
-    users = mycursor.execute('''select * from USER''')
-    return users.fetchall()
+# def get_users():
+#     users = mycursor.execute('''select * from USER''')
+#     return users.fetchall()
