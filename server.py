9aacaa82b1c2913082ab5b5be2b721486f7448af
Nolan <bourrel.nolan@gmail.com>
2024-06-27 09:57:51 +0200

    push current  day infos

--- server.py
+++ server.py
@@ -107,12 +107,25 @@ def results_user(user_pseudo):
         activity_fav = get_single_value2("SELECT activity_name FROM ACTIVITY INNER JOIN ACTIVITY_DAY ON ACTIVITY.activity_id = ACTIVITY_DAY.activity_id INNER JOIN DAY ON ACTIVITY_DAY.day_id = DAY.day_id INNER JOIN USER ON DAY.user_id = USER.user_id WHERE USER.user_pseudo = %s GROUP BY activity_name ORDER BY COUNT(*) DESC LIMIT 1", (user_pseudo,))
         activity_fav_week = get_single_value2("SELECT activity_name FROM ACTIVITY INNER JOIN ACTIVITY_DAY ON ACTIVITY.activity_id = ACTIVITY_DAY.activity_id INNER JOIN DAY ON ACTIVITY_DAY.day_id = DAY.day_id INNER JOIN USER ON DAY.user_id = USER.user_id WHERE USER.user_pseudo = %s AND DAY.day_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) GROUP BY activity_name ORDER BY COUNT(*) DESC LIMIT 1", (user_pseudo,))
         activity_fav_month = get_single_value2("SELECT activity_name FROM ACTIVITY INNER JOIN ACTIVITY_DAY ON ACTIVITY.activity_id = ACTIVITY_DAY.activity_id INNER JOIN DAY ON ACTIVITY_DAY.day_id = DAY.day_id INNER JOIN USER ON DAY.user_id = USER.user_id WHERE USER.user_pseudo = %s AND DAY.day_date >= DATE_SUB(CURDATE(), INTERVAL 31 DAY) GROUP BY activity_name ORDER BY COUNT(*) DESC LIMIT 1", (user_pseudo,))
-       
+
+        mycursor.execute("SELECT * FROM DAY INNER JOIN USER ON DAY.user_id = USER.user_id WHERE USER.user_pseudo = %s AND DAY.day_date = CURDATE()", (user_pseudo,))
+        day_infos = mycursor.fetchone()
+        current_day_comment_plus = day_infos[1]
+        current_day_comment_minus = day_infos[2]
+        current_day_mood = float(day_infos[3])
+        current_day_drinks = float(day_infos[4])
+        current_day_sleep = float(day_infos[5])
+        current_day_date = day_infos[6]
+
+        mycursor.execute("SELECT activity_name FROM ACTIVITY INNER JOIN ACTIVITY_DAY ON ACTIVITY.activity_id = ACTIVITY_DAY.activity_id INNER JOIN DAY ON ACTIVITY_DAY.day_id = DAY.day_id INNER JOIN USER ON DAY.user_id = USER.user_id WHERE user_pseudo = %s AND DAY.day_date = CURDATE()", (user_pseudo,))
+        day_activity = mycursor.fetchone()
+        current_day_activity = day_activity[0]
         return render_template('dailynotes.html', mood_avg=mood_avg, mood_max=mood_max, mood_min=mood_min, sleep_avg=sleep_avg, sleep_max=sleep_max, sleep_min=sleep_min, drinks_avg=drinks_avg, drinks_max=drinks_max, drinks_min=drinks_min,
                                mood_avg_month=mood_avg_month, mood_max_month=mood_max_month, mood_min_month=mood_min_month, sleep_avg_month=sleep_avg_month, sleep_max_month=sleep_max_month, sleep_min_month=sleep_min_month, drinks_avg_month=drinks_avg_month, drinks_max_month=drinks_max_month, drinks_min_month=drinks_min_month,
                                mood_avg_week=mood_avg_week , mood_max_week=mood_max_week, mood_min_week=mood_min_week, sleep_avg_week=sleep_avg_week, sleep_max_week=sleep_max_week, sleep_min_week=sleep_min_week, drinks_avg_week=drinks_avg_week, drinks_max_week=drinks_max_week, drinks_min_week=drinks_min_week,
                                user_pseudo=user_pseudo,
                                mood_max_days_date=mood_max_days_date, mood_min_days_date=mood_min_days_date, sleep_max_days_date=sleep_max_days_date, sleep_min_days_date=sleep_min_days_date, drinks_max_days_date=drinks_max_days_date,drinks_min_days_date=drinks_min_days_date,
-                               activity_fav=activity_fav, activity_fav_week=activity_fav_week, activity_fav_month=activity_fav_month
+                               activity_fav=activity_fav, activity_fav_week=activity_fav_week, activity_fav_month=activity_fav_month,
+                               current_day_mood=current_day_mood, current_day_drinks=current_day_drinks, current_day_sleep=current_day_sleep, current_day_date=current_day_date, current_day_comment_plus=current_day_comment_plus, current_day_comment_minus=current_day_comment_minus, current_day_activity=current_day_activity
                                )
    
